d4485a8e4aa344da36008e34fa85faf10c277445
Apirak Boonthong <apirak01@gmail.com>
2021-03-18 11:01:38 +0700

    Update O365BoxsizeQuery.ps1

--- O365BoxsizeQuery.ps1
+++ O365BoxsizeQuery.ps1
@@ -1,1 +1,13 @@
-get-mailbox -ResultSize unlimited
+get-mailbox -ResultSize unlimited | foreach {
+$MailUser = $_.Distinguishedname
+$Mailadd = $_.UserPrincipalName
+$stats= Get-MailboxStatistics $MailUser
+$usagelo = $_.Office
+$boxtype =$_.RecipientTypeDetails
+New-Object -TypeName PSObject -Property @{
+DisplayName = $stats.DisplayName
+MailboxSize = [math]::Round(($stats.TotalItemSize.ToString().Split("(")[1].Split(" ")[0].Replace(",","")/1GB),2)
+Email = $Mailadd
+MailboxType = $boxtype
+Office = $usagelo
+}} | xport-Csv -NoTypeInformation -Path "C:\temp\mailbox_size_gb.csv"
